4245a02953d250cbcf223527c03aad946f55bd1d
Manuel Epitacio Mora <MoraHernandez@imac-de-manuel.local>
2016-03-21 22:13:21 -0600

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Manuel Epitacio Mora on 21/03/16.
+//  Copyright © 2016 Manuel Epitacio Mora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
